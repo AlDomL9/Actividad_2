aed09103693b292147ff1cab6ab676e33de0b37e
JaredFG <72062480+JaredFG@users.noreply.github.com>
2021-09-14 14:27:31 -0500

    comida

--- Snake.py
+++ Snake.py
@@ -17,7 +17,11 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+foodN = vector(0, 0)
+global mouseclickx 
+global mouseclicky
 
+foodNew = True
 
 def change(x, y):
     "Change snake direction."
@@ -29,8 +33,21 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+    
+def findcoords(x,y):
+    
+    
+    mouseclickx = x
+    mouseclicky = y
+    foodN.x = int(int(x)/10)*10
+    foodN.y = int(int(y)/10)*10
+    #square(mouseclickx,mouseclicky, 9, 'green')
+    update()
 
 def move():
+    print(food.x,food.y)
+    print(foodN.x,foodN.y)
+     
     "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
@@ -46,6 +63,13 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+    
+        
+    elif head == foodN:
+        print('Snake:', len(snake))
+        foodN.x = randrange(-15, 15) * 10
+        foodN.y = randrange(-15, 15) * 10
+        
     else:
         snake.pop(0)
 
@@ -53,8 +77,9 @@ def move():
 
     for body in snake:
         square(body.x, body.y, 9, 'black')
-
+    
     square(food.x, food.y, 9, 'green')
+    square(foodN.x,foodN.y, 9, 'green')
     update()
     ontimer(move, 100)
 
@@ -67,20 +92,11 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+#newx, newy = canvas.winfo_pointerxy()
+#onclick(food(x,y))
+
+#onscreenclick(food)
+onscreenclick(findcoords,1)
 move()
 done()
-Logo
 
-Free Python Games
-
-
-Donate
-If you or your organization uses Free Games, consider donating:
-
-Donate to Free Python Games
-
-Related Topics
-Documentation overview
-Previous: Guess
-Next: Crypto
-Quick search
